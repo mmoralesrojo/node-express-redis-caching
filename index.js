ea7f21abe80ca6c323a0d2c45417cce69c9ce6a0
unknown <mmoralesrojo0901@gmail.com>
2021-12-06 22:06:50 -0500

    first commit

---
+++ index.js
@@ -0,0 +1,51 @@
+const express = require('express');
+const fetch = require('node-fetch');
+const redis = require('redis');
+
+const PORT = process.env.PORT || 5000;
+const REDIS_PORT = process.env.REDIS_PORT || 6379;
+
+const client = redis.createClient(REDIS_PORT);
+
+const app = express();
+
+function setResponse(username, repos) {
+    return `<h2>${username} has ${repos} Github repos</h2>`
+}
+
+//Make request to github for date
+async function getRepos(req, res, next) {
+    try {
+        console.log('Fetching data...');
+        const { username } = req.params;
+
+        const response = await fetch(`https://api.github.com/users/${username}`);
+        const data = await response.json();
+        const repos = data.public_repos;
+        client.setex(username, 3600, repos);
+        res.send(setResponse(username, repos));
+    }
+    catch (err) {
+        console.error(err);
+        res.status(500);
+    }
+}
+
+function cache(req, res, next) {
+    const { username } = req.params;
+    client.get(username, function (err, data) {
+        if (err) throw err;
+        if (data !== null) {
+            res.send(setResponse(username, data));
+        }
+        else {
+            next();
+        }
+    });
+}
+
+app.get('/repos/:username', cache, getRepos);
+
+app.listen(PORT, function () {
+    console.log(`Server is running on port ${PORT}`)
+})
